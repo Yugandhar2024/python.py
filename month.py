58fddce1097d635495b0e90929d54a246ba73e1d
Yugandhar2024 <kunasaniyugandhar2004@gmail.com>
2025-03-12 19:51:17 +0530

    Create month.py

---
+++ month.py
@@ -0,0 +1,11 @@
+month=int(input("Enter the month number:"))
+if(month==3 or month==4 or month==5):
+    print("Summmer Season")
+elif(month==6 or month==7 or month==8):
+    print("Rainly Season")
+elif(month==9 or month==10 or month==11):
+    print("Spring Season")
+elif(month==1 or month==2 or month==12):
+    print("Winter Season")
+else:
+    print("Invalid month number")
